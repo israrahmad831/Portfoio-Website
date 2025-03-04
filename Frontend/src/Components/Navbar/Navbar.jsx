8991ded0305ae7b51bfa2421653ea977df23650d
Israr Ahmad <154470627+israrahmad831@users.noreply.github.com>
2025-03-04 22:59:30 +0500

    NavBar Design Completed

--- Frontend/src/Components/Navbar/Navbar.jsx
+++ Frontend/src/Components/Navbar/Navbar.jsx
@@ -1,24 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { MdLightMode, MdDarkMode } from "react-icons/md"; // Import both icons
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
+    if (storedTheme === "dark") {
+      setDarkMode(true);
+      document.body.classList.add(styles.dark);
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    if (darkMode) {
+      document.body.classList.remove(styles.dark);
+      localStorage.setItem("theme", "light");
+    } else {
+      document.body.classList.add(styles.dark);
+      localStorage.setItem("theme", "dark");
+    }
+    setDarkMode(!darkMode);
+  };
+
   return (
-    <>
-    <div className="flex flex-row justify-between w-full h-full">
-      <div className="w-full h-full pt-3 pl-3">
-        logo
-      </div>
-      <div className="w-full h-full flex justify-end pt-3">
-        <ul className="flex flex-row gap-5 pr-3">
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-          <li>Experience</li>
-          <li>Projects</li>
-          <li>Resume</li>
+    <div className={`${styles.navbar} ${darkMode ? styles.dark_navbar : ""}`}>
+      <div className={styles.logo}>IsrarDev</div>
+      <div className={styles.elements}>
+        <ul className={styles.elements_list}>
+          <li className={styles.elements_type}>Home</li>
+          <li className={styles.elements_type}>About</li>
+          <li className={styles.elements_type}>Experience</li>
+          <li className={styles.elements_type}>Projects</li>
+          <li className={styles.elements_type}>Resume</li>
+          <li className={styles.elements_contact}>Contact</li>
+          <li className={styles.color_mode} onClick={toggleTheme}>
+            {darkMode ? <MdLightMode /> : <MdDarkMode />}
+          </li>
         </ul>
       </div>
     </div>
-    </>
   );
 };
 
